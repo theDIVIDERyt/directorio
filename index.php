d8be0c76830c5336fecbc632011e7ab299db3b84
theDIVIDERyt <60489931+theDIVIDERyt@users.noreply.github.com>
2020-02-21 08:36:54 -0600

    Add files via upload

--- index.php
+++ index.php
@@ -1,6 +1,5 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -8,175 +7,189 @@
     <title>Directorio</title>
     <link rel="stylesheet" type="text/css" href="estilos.css">
     <script src="https://kit.fontawesome.com/5637dd924f.js" crossorigin="anonymous"></script>
-    <script src="./scripts.js"></script>
+    <script src="./scripts.js"></script>  
 </head>
-
 <body>
-    <div class="header">
-        <h1>Directorio</h1>
-        <button type="button" class="button" onclick="abrirNuevoContacto()">Nuevo Contacto</button>
-    </div>
+<div class="header">
+<h1>Directorio</h1>
+<button type="button" onclick="mostrarModal()">Nuevo Contacto</button>
+</div>
 
+<?php 
+include "conexion.php";
+?>
+
+<section class="botonera">
     <?php
-    include "conexion.php";
-    ?>
 
-    <section class="botonera">
-        <?php
-
-        for ($i = 65; $i <= 90; $i++) {
-            echo "<button type='button' onClick=mostrarResultados('" . chr($i) . "')>" . chr($i) . "</button>";
+        for ($i=65; $i<=90; $i++){
+            echo "<button type='button' onClick=mostrarResultados('".chr($i)."')>".chr($i)."</button>";
         }
-        ?>
-    </section>
+    ?>
+</section>
 
-    <section class="busquedas">
-        <form method="post" action="index.php">
-            <input type="text" class="campo" name="busqueda" />
-            <button type="submit" class="boton"><i class="fas fa-search"></i></button>
-        </form>
-    </section>
+<section class="busquedas">
+    <form method="post" action="index.php">
+        <input type="text" class="campo" name="busqueda"/>
+        <button type="submit" class="boton"><i class="fas fa-search"></i></button>
+    </form>
+</section>
 
-    <?php
+<?php
     //checamos si se ha enviado un querystring a la página o el formulario con una búsqueda
-    if (isset($_REQUEST["letra"])) {
+    if (isset($_REQUEST["letra"])){
         $letraParaBuscar = $_REQUEST["letra"];
 
         //buscamos los apellidos que inician con la letra seleccionada
-        $sql = "select idDirectorio, nombre, apellido from andres_directorio where apellido like '" . $letraParaBuscar . "%' order by apellido";
+        $sql = "select idDirectorio, nombre, apellido from paulina_directorio where apellido like '".$letraParaBuscar."%' order by apellido";
         $rs = ejecutar($sql);
-    } else if (isset($_POST["busqueda"])) {
+
+    }else if (isset($_POST["busqueda"])){
         $registroParaBuscar = $_POST["busqueda"];
 
-        $sql = "select idDirectorio, nombre, apellido from andres_directorio where apellido like '" . $registroParaBuscar . "%' order by apellido";
+        $sql = "select idDirectorio, nombre, apellido from paulina_directorio where apellido like '".$registroParaBuscar."%' order by apellido";
         $rs = ejecutar($sql);
     }
-    ?>
+?>
 
-    <section class="listaResultados">
-        <div class="contenedor" id="contenedor">
-            <?php
-            if (isset($_REQUEST["letra"]) || isset($_POST["busqueda"])) {
-                echo '<div id="r1">Registros encontrados: </div>';
-                echo '<ul class="listaNombres">';
-
-                //checamos si la búsqueda realizada encontró registros en la BD
-                if (mysqli_num_rows($rs) != 0) {
-                    $k = 0;
-                    while ($datos = mysqli_fetch_array($rs)) {
-                        if ($k % 2 == 0) {
-                            echo "<li class='oscuro'>";
-                        } else {
-                            echo "<li class='claro'>";
-                        }
-                        echo "<a href='javascript:mostrarRegistro(" . $datos['idDirectorio'] . ")'>" . $datos["apellido"] . "</a>, " . $datos["nombre"] . "</li>";
-                        $k++;
-                    }
-                } else {
-                    echo 'No se encontraron registros con la búsqueda realizada';
-                }
-
-                echo "</ul>";
-            } else if (isset($_REQUEST["id"])) {
-                // checamos si se ha enviado un id para buscar un registro en particular
-                $id = $_REQUEST["id"];
-
-                //hacemos un query para obtener toda la información del registro que se desea deplegar
-                $sql = "select * from andres_directorio where idDirectorio =" . $id;
-
-                //ejecutamos el query
-                $rs = ejecutar($sql);
-
-                $datosRegistro = mysqli_fetch_array($rs);
-            } else {
-                echo '<div id="r1">Seleccione una letra o realize una búsqueda para desplegar los registros del directorio</div>';
-            }
-            ?>
-        </div>
-
-        <div class="contenedorRegistro" id="registro">
-            <button type="button"><i class="fas fa-caret-square-left"></i></button>
-            <div class="registro">
-                <div class="cerrar">
-                    <button type="button" onClick="ocultarTarjeton()">
-                        <i class="fas fa-window-close"></i></button>
-                </div>
-                <div class="titulo"><?php echo $datosRegistro["nombre"] . " " . $datosRegistro["apellido"]; ?></div>
-
-                <div class="iconos"><i class="fas fa-building"></i></div>
-                <div class="datos"><?php echo $datosRegistro["empresa"]; ?></div>
-                <div class="foto">
-                    <?php
-                    // checamos si existe una foto para este registro. Si no existe, colocamos la imagen de no foto
-                    if ($datosRegistro["foto"] == null) {
-                        echo "<img src='fotos/noFoto.png' class='fotoRegistro'>";
-                    } else {
-                        //colocamos la foto del registro
-                    }
-                    ?>
-                </div>
-
-                <div class="iconos"><i class="fas fa-envelope"></i></div>
-                <div class="datos"><?php echo $datosRegistro["email"]; ?></div>
-
-                <div class="iconos"><i class="fas fa-phone"></i></div>
-                <div class="datos"><?php echo $datosRegistro["telefono"]; ?></div>
-
-                <div class="iconos"><i class="fas fa-comment"></i></div>
-                <div class="datos"><?php echo $datosRegistro["comentarios"]; ?></div>
-
-
-            </div>
-            <button type="button"><i class="fas fa-caret-square-right"></i></button>
-
-        </div>
-
+<section class="listaResultados">
+    <div class = "contenedor" id="contenedor">
         <?php
-        if (isset($_REQUEST["id"])) {
-            echo '<script language="javascript">mostrarDatosIndividuales()</script>';
+        if (isset($_REQUEST["letra"]) || isset($_POST["busqueda"])){
+            echo '<div id="r1">Registros encontrados: </div>';
+            echo '<ul class="listaNombres">';
+
+            //checamos si la búsqueda realizada encontró registros en la BD
+             if (mysqli_num_rows($rs) != 0){
+                $k = 0;
+                while ($datos = mysqli_fetch_array($rs)){
+                    if ($k % 2 == 0){
+                        echo "<li class='oscuro'>";
+                    }else{
+                        echo "<li class='claro'>";
+                    }
+                    echo "<a href='javascript:mostrarRegistro(".$datos['idDirectorio'].")'>".$datos["apellido"]."</a>, ".$datos["nombre"]."</li>";
+                    $k++;
+                }
+            }else{
+                echo 'No se encontraron registros con la búsqueda realizada';
+            }
+
+            echo "</ul>";
+
+        }else if (isset($_REQUEST["id"])){
+            // checamos si se ha enviado un id para buscar un registro en particular
+            $id = $_REQUEST["id"];
+
+            //hacemos un query para obtener toda la información del registro que se desea deplegar
+            $sql = "select * from paulina_directorio where idDirectorio =".$id;
+
+            //ejecutamos el query
+            $rs = ejecutar($sql);
+
+            $datosRegistro = mysqli_fetch_array($rs);
+
+        } else {
+            echo '<div id="r1">Seleccione una letra o realize una búsqueda para desplegar los registros del directorio</div>';
         }
-        ?>
-
-
-    </section>
-
-    <div class="modal" id="modal">
-        <div class="modal-bg">
-            <div class="modal-container">
-                <button type="button" onclick="cerrarNuevoContacto()">Cerrar modal</button>
-
-                <body>
-                    <form>
-                        <fieldset>
-                            <legend>Ingresa tus datos</legend>
-                            <p>
-                                <label for="first_name">Nombre: *</label>
-                                <input name="first_name" type="text" required placeholder="Ingresa tu nombre">
-                            </p>
-                            <p>
-                                <label for="last_name">Apellido: *</label>
-                                <input name="last_name" type="text" required placeholder="Ingresa tu apellido">
-                            </p>
-                            <p>
-                                <label for="email">Dirección de E-mail: *</label>
-                                <input name="email" type="email" required placeholder="Ingresa tu correo">
-                            </p>
-                            <p>
-                                <label for="telefono">Número de teléfono:</label>
-                                <input name="telefono" type="number" required placeholder="Ingresa tu número telefónico">
-                            </p>
-                        </fieldset>
-                    </form>
-            </div>
-        </div>
+        ?>  
+        
     </div>
 
+    <div class="contenedorRegistro" id="registro">
+        <button type="button"><i class="fas fa-caret-square-left"></i></button>
+        <div class="registro">
+            <div class="cerrar">
+                <button type="button" onClick="ocultarTarjeton()">
+                    <i class="fas fa-window-close"></i></button>
+            </div>
+            <div class="titulo"><?php echo $datosRegistro["nombre"]." ".$datosRegistro["apellido"];?></div>
+            
+            <div class="iconos"><i class="fas fa-building"></i></div>
+            <div class="datos"><?php echo $datosRegistro["empresa"];?></div>
+            <div class="foto">
+                <?php
+                // checamos si existe una foto para este registro. Si no existe, colocamos la imagen de no foto
+                if ($datosRegistro["foto"] == null){
+                    echo "<img src='fotos/noFoto.png' class='fotoRegistro'>";
+                }else{
+                    //colocamos la foto del registro
+                }
+                ?>
+            </div>
+
+            <div class="iconos"><i class="fas fa-envelope"></i></div>
+            <div class="datos"><?php echo $datosRegistro["email"];?></div>
+
+            <div class="iconos"><i class="fas fa-phone"></i></div>
+            <div class="datos"><?php echo $datosRegistro["telefono"];?></div>
+
+            <div class="iconos"><i class="fas fa-comment"></i></div>
+            <div class="datos"><?php echo $datosRegistro["comentarios"];?></div>
+            
+
+        </div>
+        <button type="button"><i class="fas fa-caret-square-right"></i></button>
+            
+    </div>
+    
     <?php
-    if (isset($_REQUEST["id"])) {
-        echo '<script language="javascript">abrirNuevoContacto()</script>';
+    if (isset($_REQUEST["id"])){
+        echo '<script language="javascript">mostrarDatosIndividuales()</script>';
     }
     ?>
+    
+
+</section>
+
+<div class="modal" id="formulario">
+    <div class="modal-bg">
+        <form method="post">
+        <div class="modal-container"> 
+            <div class="cerrar">
+                <button type="button" onclick="ocultarModal()" class="botonCerrarForm"><i class="fas fa-window-close"></i></button>
+            </div>
+            <h2 class="nuevoRegistro">Ingreso Nuevo Registro</h2>
+            <div class="iconoForm"><i class="fas fa-user"></i></div>
+            <p class="campo">
+                <label for="first_name"></label><br>
+                <input class="celda" id="nombre" name="first_name" placeholder="Nombre" type="text">
+            </p>
+            <div class="iconoForm"><i class="fas fa-user"></i></div>
+            <p class="campo">
+                <label for="last_name"></label><br>
+                <input class="celda" id="apellido" name="last_name" placeholder="Apellido" type="text">
+            </p>
+            <div class="iconoForm"><i class="fas fa-building"></i></div>
+            <p class="campo">
+                <label for="empresa"></label><br>
+                <input class="celda" id="empresa" name="empresa" placeholder="Empresa" type="text">
+            </p>
+            <div class="iconoForm"><i class="fas fa-envelope"></i></div>
+            <p class="campo">
+                <label for="email"></label><br>
+                <input class="celda" id="email" name="email" placeholder="E-mail" type="text">
+            </p>
+            <div class="iconoForm"><i class="fas fa-phone"></i></div>
+            <p class="campo">
+                <label for="telephone"></label><br>
+                <input class="celda" id="telefono" name="telephone" placeholder="Teléfono" type="number">
+            </p>
+            <div class="iconoForm"><i class="fas fa-comment"></i></div>
+            <p class="campo">
+                <label for="comments"></label><br>
+                <textarea class="celdaComentarios" name="comments" maxlength="500" cols="40" rows="5"></textarea>
+            </p>
+            <input type="button" class="ingresar" onclick="validarFormulario()" value="Ingresar">
+        </form>
+        <ul class="mensajeError" id="msj">
+            
+        </ul>    
+        </div>
+    </div>
+</div>
+
+
 
 </body>
-
 </html>
--- scripts.js
+++ scripts.js
@@ -1,30 +1,32 @@
-function mostrarRegistro(id) {
-    document.location.assign("index.php?id=" + id);
+function mostrarRegistro(id){
+    document.location.assign("index.php?id="+id);
 }
 
-function mostrarResultados(letraEscogida) {
-    document.location.assign("index.php?letra=" + letraEscogida);
+function mostrarResultados(letraEscogida){
+    document.location.assign("index.php?letra="+letraEscogida);
 }
 
-function mostrarDatosIndividuales() {
+function mostrarDatosIndividuales(){
     document.getElementById("contenedor").style.display = "none";
     document.getElementById("registro").style.display = "flex";
 }
 
-function ocultarTarjeton() {
+function ocultarTarjeton(){
     //recarga la página
     document.location.assign("index.php");
 }
 
-function abrirNuevoContacto() {
-    document.getElementById("modal").style.display = "block";
+function mostrarModal(){
+    document.getElementById("contenedor").style.display = "none";
+    document.getElementById("formulario").style.display = "block";
 }
 
-function cerrarNuevoContacto() {
-    document.getElementById("modal").style.display = "none";
+function ocultarModal(){
+    document.getElementById("contenedor").style.display = "block";
+    document.getElementById("formulario").style.display = "none";
 }
 
-function validarFormulario() {
+function validarFormulario(){
     var nombre, apellido, email, empresa;
     nombre = document.getElementById("nombre").value;
     apellido = document.getElementById("apellido").value;
@@ -33,7 +35,7 @@ function validarFormulario() {
     var mensaje = new Array();
     var flagCampos = false;
 
-    if (nombre == "" || apellido == "" || email == "" || empresa == "") {
+    if(nombre == "" || apellido == "" || email == "" || empresa == ""){
         flagCampos = true;
         mensaje.push("Llene todos los campos");
     }
@@ -44,70 +46,70 @@ function validarFormulario() {
     var flagUltimoPunto = false;
     var flagNumCaracteres = false;
     var n = email.length;
-    if (n < 6) {
+    if (n < 6){
         flagNumCaracteres = true;
     }
 
-
+   
     var primerCaracter = email.charCodeAt(0);
-    if ((primerCaracter >= 65 && primerCaracter >= 90) || (primerCaracter >= 97 && primerCaracter <= 122)) {
+    if ((primerCaracter>= 65 && primerCaracter>= 90) || (primerCaracter>= 97 && primerCaracter<=122)){
         //no hacemos nada, esta correcto
-    } else {
+    }else{
         flagPrimerCaracter = true;
     }
 
-
+   
     var numArrobas = 0;
-    for (var i = 0; i < n; i++) {
-        if (email.charAt(i) == "@") numArrobas++;
+    for (var i=0; i<n; i++){ 
+        if(email.charAt(i) == "@") numArrobas++;
     }
-    if (numArrobas != 1) {
-        flagArroba = true;
+        if(numArrobas != 1){
+            flagArroba= true;
     }
 
-
-    if (!flagArroba) {
+   
+    if (!flagArroba){
         var posArroba = email.indexOf("@");
-        if (posArroba >= 1 && posArroba <= email.length - 5) {
+        if (posArroba >= 1 && posArroba <= email.length-5){
             //correcto
-        } else {
+        }else{
             flagPosicionArroba = true;
         }
     }
-
+   
     var ultimoPunto = email.lastIndexOf(".");
-    if ((ultimoPunto >= email.length - 5 && ultimoPunto <= email.length - 3) && ultimoPunto != 1) {
+    if((ultimoPunto >= email.length-5 && ultimoPunto<= email.length-3) && ultimoPunto!= 1){
         //correcto
-    } else {
+    }else{
         flagUltimoPunto = true;
     }
 
-    if ((flagUltimoPunto) || (flagNumCaracteres) || (flagPosicionArroba) || (flagPrimerCaracter) || (flagArroba)) {
+    if((flagUltimoPunto) || (flagNumCaracteres) || (flagPosicionArroba) || (flagPrimerCaracter) || (flagArroba)){
         mensaje.push("El email ingresado es inválido");
     }
 
-    if (!flagCampos && !flagUltimoPunto && !flagNumCaracteres && !flagPosicionArroba && !flagPrimerCaracter && !flagArroba) {
+    if(!flagCampos && !flagUltimoPunto && !flagNumCaracteres  && !flagPosicionArroba  && !flagPrimerCaracter && !flagArroba){
         //borramos los mensajes de la lista de errores si es que existen
-        document.getElementById("msj").innerHTML = "";
+        document.getElementById("msj").innerHTML="";
 
         //añadimos a la lista de errores, un elemento que indica que el primer ingresado es correcto
         alert("Todo bien");
-    } else {
+    }else{
         //corremos una funcion que lee los mensajes del arreglo mensaje, y los imprime como una lista dentro del ul
         imprimirErrores(mensaje);
     }
 }
 
 
-function imprimirErrores(errores) {
+function imprimirErrores(errores){
     //borramos todos lo errores impresos anteriormente si es que existen
     var listaErrores = document.getElementById("msj");
     listaErrores.innerHTML = "";
 
     //leemos el arreglo errores y por cada uno de sus elementos creamos un elemento li que añadimos al tag ul que representa o muestra la lista de errores
-    errores.forEach(function (error) {
+    errores.forEach(function (error){
         var li = document.createElement("li");
-        li.innerHTML = "<span class = 'error'>" + error + "</span>";
+        li.innerHTML = "<span class = 'error'>"+error+"</span>";
         listaErrores.appendChild(li);
     });
 }
